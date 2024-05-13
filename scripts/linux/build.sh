09ad014286d50916694a31695f7b4566850cb1c5
STFleming <shane.t.fleming@gmail.com>
2024-05-13 10:32:13 +0100

    first cut at fully fleshing out the build script that will provision an Ubuntu 24.04 system from scratch

--- scripts/linux/build.sh
+++ scripts/linux/build.sh
@@ -4,36 +4,149 @@
 # SPDX-License-Identifier: MIT
 
 DRIVER_TARBALL=ubuntu24.04_npu_drivers.tar.gz
+MIN_KERNEL_VERSION="6.8.8+"
+NPU_FIRMWARE="/lib/firmware/amdnpu/1502_00/npu.sbin"
 
+# Check that we are on Ubuntu24.04
+distro_info=$(lsb_release -d)
+if [[ $distro_info != *"Ubuntu 24.04"* ]]; then
+	echo "Riallto is only currently supported on Ubuntu 24.04"
+	exit 1
+fi
+
+# Check that docker is installed
+if command -v docker >/dev/null 2>&1; then
+	echo "Docker has been found."
+else
+	echo "Docker could not be found on this system."
+       	echo "Unable to continue the installation."
+        echo "Please configure docker using the instructions found here:"
+	echo "https://docs.docker.com/engine/install/ubuntu/"
+	echo "And then rerun the script"	
+	exit 1
+fi
+
+############### License file check ###################
+# Check to make sure that a license file has been provided and that 
+# a MAC address can be extracted from it for adding into the docker
+# image
+if [ "$#" -ne 1 ]; then
+	echo "Usage $0 <Xilinx license file>"
+	exit 1
+fi
+LIC_FILE="$1"
+
+# Check to make sure that the license file exists
+if [ ! -f "$LIC_FILE" ]; then
+	echo "Unable to open the license file $LIC_FILE"
+	exit 1
+fi
+
+MAC=$(grep -oP 'HOSTID=\K[^;]+' $1 | head -n1 | sed 's/\(..\)/\1:/g; s/:$//')
+echo "Found a License file associated with MAC address $MAC"
+####################################################
+
+
+######### Kernel and NPU driver check / install ###########
+# Check to see if the kernel version and NPU driver is already installed
+build_kernel_and_xrt=0
+kernel_version=$(uname -r)
+
+# Check that the current kernel version is greater than the minimum version
+version_ge() {
+	test "$(echo -e "$1\n$2" | sort -V | head -n1)" != "$1"
+}
+
+if version_ge $kernel_version $MIN_KERNEL_VERSION; then
+	echo "Kernel version is okay, is NPU available?"	
+	if [ -f "${NPU_FIRMWARE}" ]; then	
+		echo "NPU is available, just setting up Riallto"
+		build_kernel_and_xrt = 0;			
+	else
+		build_kernel_and_xrt = 1
+	fi
+else
+	build_kernel_and_xrt = 1
+fi
+
+if [ $build_kernel_and_xrt -eq 1 ]; then
+	# Building the driver and kernel version and installing it
+	# First check to make sure that secure boot is disabled.
+	if mokutil --sb-state | grep -q "enabled"; then
+		echo "Secure boot is currently enabled."
+		echo "To install Riallto on Linux currently requires a" 
+		echo "non-mainline kernel version ${MIN_KERNEL_VERSION}."
+	       	echo "If you would like to continue with the installation "
+	        echo "please disable secure boot in your bios settings and rerun this script."
+		exit 1	
+	fi
+	
+	if [ ! -f "./xdna-driver-builder/${DRIVER_TARBALL}" ]; then
+		echo "xdna-driver-builder/${DRIVER_TARBALL} is missing, building it from scratch"
+		pushd xdna-driver-builder
+		./build.sh
+		popd
+	fi
+
+
+	if version_ge $kernel_version $MIN_KERNEL_VERSION; then
+		echo "To install Riallto requires upgrading your kernel to ${MIN_KERNEL_VERSION}"
+		echo "After upgrading you will have to restart your machine and rerun this script"
+		while true; do
+			read -p "Are you happy to continue? (Y/N)" answer
+			case $answer in
+				[Yy]* ) echo "You chose yes, attempting to update kernel"; break;;
+				[Nn]* ) echo "Exiting"; exit 1;;
+				* ) echo "Please chose Y or N.";;
+			esac
+		done
+			
+		kernel_bump_tmp_dir=$(mktemp -d)
+		tar -xzvf "./xdna-driver-builder/${DRIVER_TARBALL}" -C "${kernel_bump_tmp_dir}"
+		pushd $kernel_bump_tmp_dir/root/debs
+			sudo dpkg -i linux-headers*_amd64.deb
+			sudo dpkg -i linux-image*_amd64.deb
+			sudo dpkg -i linux-libc*_amd64.deb
+		popd
+		echo "Please now restart your machine and rerun the script"
+		exit 1
+	fi
+fi
+
+# Install the NPU drivers (xdna-driver)
+if [ ! -f "${NPU_FIRMWARE}" ]; then	
+	npu_install_tmp_dir=$(mktemp -d)
+	pushd $npu_install_tmp_dir/root/debs
+		sudo -E dpkg -i xrt_*-amd64-xrt.deb
+		sudo -E dpkg -i xrt_plugin*-amdxdna.deb 
+	popd
+fi
+#########################################################
+
+########### Riallto Docker image construction ###########
+echo "Building Riallto docker image" 
 ## Checks to make sure that all the required tarballs and license are in the directory 
 if [ ! -f "./pynqMLIR-AIE.tar.gz" ]; then
-	echo "Error! pynqMLIR-AIE.tar.gz is missing, downloading from opendownloads"
-	wget -O pynqMLIR-AIE.tar.gz https://www.xilinx.com/bin/public/openDownload?filename=pynqMLIR_AIE_py310_v0.9.tar.gz 
+	echo "Error! pynqMLIR-AIE.tar.gz is missing, downloading from opendownloads..."
+	wget -O pynqMLIR-AIE.tar.gz https://www.xilinx.com/bin/public/openDownload?filename=pynqMLIR_AIE_py312_v0.9.tar.gz 
 fi
 
 if [ ! -f "./xilinx_tools.tar.gz" ]; then
-	echo "xilinx_tools.tar.gz is missing, downloading it from opendownloads"
+	echo "xilinx_tools.tar.gz is missing, downloading it from opendownloads..."
 	wget -O riallto_installer.zip https://www.xilinx.com/bin/public/openDownload?filename=Riallto-v1.0.zip
 	unzip riallto_installer.zip
         mv Riallto_v1.0/Riallto/downloads/xilinx_tools_latest.tar.gz ./xilinx_tools.tar.gz	
 fi
 
-if [ ! -f "./xdna-driver-builder/${DRIVER_TARBALL}" ]; then
-	echo "xdna-driver-builder/${DRIVER_TARBALL} is missing, building it from scratch"
-	pushd xdna-driver-builder
-	./build.sh
-	popd
-fi
-
-if [ ! -f "./Xilinx.lic" ]; then
-	echo "Error! Xilinx.lic is missing"
-	exit 1
-fi
+cp $LIC_FILE Xilinx.lic
 
 tar -xzvf ./xdna-driver-builder/${DRIVER_TARBALL} -C ./
 
 docker build \
 	--build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
+	--build-arg LIC_MAC=$MAC \
        	-t riallto:latest \
 	./
+
+#####################################################
 
