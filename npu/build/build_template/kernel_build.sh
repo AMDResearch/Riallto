2e0bdd3466596e9a82af06d8a651608a10239112
Mario Ruiz <mruiznog@amd.com>
2024-06-21 12:33:20 +0100

    Include kernels

--- npu/build/build_template/kernel_build.sh
+++ npu/build/build_template/kernel_build.sh
@@ -8,6 +8,6 @@ cd $SCRIPT_DIR
 
 source /opt/mlir_settings.sh
 
-xchesscc $CHESSCC2_FLAGS -c $1.cc -o $1.o #2>&1 | tee $1.log
+xchesscc $CHESSCC2_FLAGS -I kernels -c $1.cc -o $1.o #2>&1 | tee $1.log
 
 echo "Successfully built $1.o"
