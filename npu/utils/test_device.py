fb9762c6622c48701303cc27326ade2813ea57b0
Mario Ruiz <11815099+mariodruiz@users.noreply.github.com>
2024-08-07 07:48:38 +0100

    Initial support for MCDM driver (#52)
    * Check for two NPU names
    
    * Initial commit to add support for xrt-smi
    
    * Give generic name if app does not exist
    
    * Count unknown apps
    
    * Show unknown apps
    
    * Bring nputop to Linux
    
    * Test multiload in Linux
    
    * Fix loop iterations
    
    * Remove unused import
    
    * Bring back ONNX test
    
    * Do not enable nputop in Linux yet
    
    * Revert changes

--- npu/utils/test_device.py
+++ npu/utils/test_device.py
@@ -3,14 +3,35 @@
 
 import subprocess
 
+
 def version_to_tuple(version):
     """ Convert a version string to a tuple of integers"""
     return tuple(map(int, version.split('.')))
 
+
+def get_device_name():
+    """Returns the name of the NPU device"""
+    for name in ['AMD IPU Device', 'NPU Compute Accelerator Device']:
+        command = f'''
+        $device = Get-PnpDevice | Where-Object {{ $_.FriendlyName -eq "{name}" }}
+        if ($device -eq $null) {{
+            return $null
+        }} else {{
+            return $device.Name
+        }}
+        '''
+        result = subprocess.run(['powershell', '-Command', command],
+                                capture_output=True, text=True, check=False)
+        if result.stdout.strip():
+            return result.stdout.strip()
+    return None
+
+
 def get_device_status():
-    """ Returns states of the NPU device, when operating normally should return the string 'OK'."""
+    """Returns state of the NPU device, when working 'OK' is returned"""
+    name = get_device_name()
     command = f'''
-    $device = Get-PnpDevice | Where-Object {{ $_.FriendlyName -eq "AMD IPU Device" }}
+    $device = Get-PnpDevice | Where-Object {{ $_.FriendlyName -eq "{name}" }}
     if ($device -eq $null) {{
         return $null
     }} else {{
@@ -18,17 +39,22 @@ def get_device_status():
     }}
     '''
 
-    result = subprocess.run(['powershell', '-Command', command], capture_output=True, text=True)
+    result = subprocess.run(['powershell', '-Command', command],
+                            capture_output=True, text=True, check=False)
     return result.stdout.strip()
 
+
 def get_driver_version():
+    name = get_device_name()
     command = f'''
-    $device = Get-WmiObject -Class Win32_PnPSignedDriver | Where-Object {{ $_.DeviceName -eq "AMD IPU Device" }}
+    $device = Get-WmiObject -Class Win32_PnPSignedDriver | Where-Object {{ $_.DeviceName -eq "{name}" }}
     return $device.DriverVersion
     '''
 
-    result = subprocess.run(['powershell', '-Command', command], capture_output=True, text=True)
+    result = subprocess.run(['powershell', '-Command', command],
+                            capture_output=True, text=True, check=False)
     return result.stdout.strip()
+
 
 def test_device_status():
     """ Checks if device exists, and if the status is expected 'OK'."""
@@ -44,39 +70,44 @@ def test_driver_version(min_driver_version="10.1109.8.100"):
     min_version = version_to_tuple(min_driver_version)
     version = version_to_tuple(get_driver_version())
 
-    assert version >= min_version, f"Current driver version: {version}, expected minimum: {min_version}"
+    assert version >= min_version, \
+        f"Current driver version: {version}, expected minimum: {min_version}"
+
 
 def reset_npu():
-    """ This is a utility function that wraps a powershell script to 
+    """ This is a utility function that wraps a powershell script to
     reset the IPU device.
     Note: this requires admin privileges.
     """
-    
-    command = '''
-    $deviceName = "AMD IPU Device"
-    $device = Get-WmiObject Win32_PnPEntity | Where-Object { $_.Name -eq $deviceName }
+
+    name = get_device_name()
+
+    command = f'''
+    $deviceName = "{name}"
+    $device = Get-WmiObject Win32_PnPEntity | Where-Object {{ $_.Name -eq $deviceName }}
 
     # Disable IPU
     $result = $device.Disable()
-    if ($result.ReturnValue -eq 0) {
+    if ($result.ReturnValue -eq 0) {{
         Write-Host "Disabled $deviceName"
-    } else {
+    }} else {{
         Write-Host "Failed to disable $deviceName"
         exit 1
-    }
+    }}
 
-    # Need to wait a bit so it doesn't go into a bad state and then we have to restart...
+    # Wait a bit so it doesn't go into a bad state and then we restart...
     Start-Sleep -Seconds 5
 
     # Enable IPU
     $result = $device.Enable()
-    if ($result.ReturnValue -eq 0) {
+    if ($result.ReturnValue -eq 0) {{
         Write-Host "Enabled $deviceName"
-    } else {
+    }} else {{
         Write-Host "Failed to enable $deviceName"
         exit 1
-    }
+    }}
     '''
 
-    result = subprocess.run(['powershell', '-Command', command], capture_output=True, text=True)
+    result = subprocess.run(['powershell', '-Command', command],
+                            capture_output=True, text=True, check=False)
     return result
