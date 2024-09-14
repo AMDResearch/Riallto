12b0821335a0e90c31edd5f5c5aae6a1495eb038
Mario Ruiz <mruiznog@amd.com>
2024-09-14 18:14:13 +0100

    Add multi buffer tests from IT

---
+++ tests/test_viz_multibuffer.py
@@ -0,0 +1,134 @@
+# Copyright (C) 2023 Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+from pathlib import Path
+import pytest
+import numpy as np
+from npu.build.kernel import Kernel
+from npu.build.mtkernel import MTPassThrough, MTSplit, MTConcat
+from npu.build.appbuilder import AppBuilder
+
+
+imgdir = str(Path(__file__).parent / "images") + '/'
+
+
+k_2in_1out = """
+#include "aie_api/aie.hpp"
+extern "C" {
+    void aie_kernel_2_1(uint16_t* in0, uint16_t* in1, uint16_t* out_buffer) {
+        auto vec_in0 = ::aie::load_v<32>(in0);
+        auto vec_in1 = ::aie::load_v<32>(in1);
+        in0 += 32;
+        in1 += 32;
+        ::aie::store_v(out_buffer, vec_in0);
+        out_buffer += 32;
+        ::aie::store_v(out_buffer, vec_in1);
+        out_buffer += 32;
+    }
+}
+"""
+
+
+k_1in_2out = """
+#include "aie_api/aie.hpp"
+extern "C" {
+    void aie_kernel_1_2(uint16_t* in_buffer, uint16_t* out0, uint16_t* out1) {
+        auto vec_in = ::aie::load_v<32>(in_buffer);
+        in_buffer += 32;
+        ::aie::store_v(out0, vec_in);
+        out0 += 32;
+        vec_in = ::aie::load_v<32>(in_buffer);
+        in_buffer += 32;
+        ::aie::store_v(out1, vec_in);
+        out1 += 32;
+    }
+}
+"""
+
+
+k_2in_2out = """
+#include "aie_api/aie.hpp"
+extern "C" {
+    void aie_kernel_2_2(uint16_t* in0, uint16_t* in1, uint16_t* out0, uint16_t* out1) {
+        auto vec_in0 = ::aie::load_v<32>(in0);
+        in0 += 32;
+        ::aie::store_v(out1, vec_in);
+        out1 += 32;
+        auto vec_in1 = ::aie::load_v<32>(in1);
+        in1 += 32;
+        ::aie::store_v(out0, vec_in);
+        out0 += 32;
+    }
+}
+"""
+
+
+def test_viz_2in_1out():
+    def kernel_behavior(invobj):
+        invobj.out_buffer.array = np.concat(invobj.in0.array, invobj.in1.array)
+
+    class SingleKernel_2_1(AppBuilder):
+        def __init__(self):
+            self.kernel = Kernel(k_2in_1out, kernel_behavior)
+            super().__init__()
+
+        def callgraph(self, x_in0, x_in1, x_out):
+            x_out[:] = self.kernel(x_in0, x_in1)
+
+
+    x_in0 = np.zeros(shape=(32, 1), dtype=np.uint16)
+    x_in1 = np.zeros(shape=(32, 1), dtype=np.uint16)
+    x_out = np.zeros(shape=(64, 1), dtype=np.uint16)
+
+    app_builder = SingleKernel_2_1()
+    _ = app_builder.to_metadata(x_in0, x_in1, x_out)
+    app_builder.save(svgfile := f'{imgdir}{app_builder.name}.svg')
+    #assert _count_class_occurrences(svgfile, 'kernel') == 2
+    #assert _count_class_occurrences(svgfile, 'aie_tile_buffers') == 4
+
+
+def test_viz_1in_2out():
+    def kernel_behavior(invobj):
+        invobj.out0.array = invobj.in_buffer.array
+        invobj.out1.array = invobj.in_buffer.array
+
+    class SingleKernel_1_2(AppBuilder):
+        def __init__(self):
+            self.kernel = Kernel(k_1in_2out, kernel_behavior)
+            super().__init__()
+
+        def callgraph(self, x_in, x_out0, x_out1):
+            x_out0[:], x_out1[:] = self.kernel(x_in)
+
+
+    x_in = np.zeros(shape=(64, 1), dtype=np.uint16)
+    x_out0 = np.zeros(shape=(32, 1), dtype=np.uint16)
+    x_out1 = np.zeros(shape=(32, 1), dtype=np.uint16)
+
+    app_builder = SingleKernel_1_2()
+    _ = app_builder.to_metadata(x_in, x_out0, x_out1)
+    app_builder.save(svgfile := f'{imgdir}{app_builder.name}.svg')
+
+
+def test_viz_2in_2out():
+    def kernel_behavior(invobj):
+        invobj.out1.array = invobj.in0.array
+        invobj.out0.array = invobj.in1.array
+
+    class SingleKernel_1_2(AppBuilder):
+        def __init__(self):
+            self.kernel = Kernel(k_2in_2out, kernel_behavior)
+            super().__init__()
+
+        def callgraph(self, x_in0, x_in1, x_out0, x_out1):
+            x_out0[:], x_out1[:] = self.kernel(x_in0, x_in1)
+
+
+    x_in0 = np.zeros(shape=(32, 1), dtype=np.uint16)
+    x_in1 = np.zeros(shape=(32, 1), dtype=np.uint16)
+    x_out0 = np.zeros(shape=(32, 1), dtype=np.uint16)
+    x_out1 = np.zeros(shape=(32, 1), dtype=np.uint16)
+
+    app_builder = SingleKernel_1_2()
+    _ = app_builder.to_metadata(x_in0, x_in1, x_out0, x_out1)
+    app_builder.save(svgfile := f'{imgdir}{app_builder.name}.svg')
