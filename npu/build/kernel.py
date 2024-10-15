9af6738c7c8542fb24124c13596733248d39ae8a
Mario Ruiz <mruiznog@amd.com>
2024-10-15 07:43:05 +0100

    Add breakline

--- npu/build/kernel.py
+++ npu/build/kernel.py
@@ -101,7 +101,7 @@ class Kernel(KernelMeta):
         """Verify that extern C is used"""
         tight_code = self.srccode.replace(' ', '').replace('	', '')
         if 'extern"C"' not in tight_code or '//extern"C"' not in tight_code:
-            raise SyntaxError('extern "C" not found. Top level function '
+            raise SyntaxError('extern "C" not found. Top level function ' \
                               'should be wrapped by extern "C"')
 
     def display(self)->None:
