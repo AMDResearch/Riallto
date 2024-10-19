2401298e58e5293c65f04a1a9d2cdf56df4da971
Mario Ruiz <mruiznog@amd.com>
2024-10-19 10:24:59 +0100

    Try to fix color

--- npu/utils/appviz.py
+++ npu/utils/appviz.py
@@ -196,11 +196,8 @@ class AppViz:
             else:
                 bufcol = self._dbuf_colors[c['name']]
 
-            if dst.get('mtmode') == 'passthrough':
-                dst_it_color = _mt2it_color[self._ct2mt_counter//2]
-            else:
-                idx = int(c['sinkkernel'][-1]) % 2
-                dst_it_color = _mt2it_color[idx]
+            idx = (int(c['sinkkernel'][-1]) + 1) % 2
+            dst_it_color = _mt2it_color[idx]
 
             self._col_svg.mem_tiles[0].add_buffer(
                         bufcol,
